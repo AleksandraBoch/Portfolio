a8860f3b93486703ef280dd0f8cf1b6af67a60d1
AleksandraBoch <shemaFoxy@gmail.com>
2023-02-05 19:21:58 +0300

    р

---
+++ src/nav/nav.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navi=()=>{
+    return (
+        <div>
+            <div>My skills</div>
+            <div>my works</div>
+            <div>contact me</div>
+            <div></div>
+        </div>
+    )
+
+}
