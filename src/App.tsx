6cf390d182ed10169f91dd81e81858eb426fb893
AleksandraBoch <shemaFoxy@gmail.com>
2023-12-19 15:01:47 +0300

    portfolio changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,19 @@ import {MainBlock} from "./mainBlock/Main";
 import {SkillsBlock} from "./mainBlock/skills/skillsBlock";
 import {MyWorks} from "./myWorks/MyWorks";
 import {Contacts} from "./Contacts/Contacts";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return (
         <div className={'App_main'}>
             <header>
                 <Header/>
-                <MainBlock/>
-                <SkillsBlock/>
-                <MyWorks/>
-                <Contacts/>
+                <Routes>
+                    <Route path="/main" element={<MainBlock />} />
+                    <Route path="/skills" element={<SkillsBlock />} />
+                    <Route path="/works" element={<MyWorks />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                </Routes>
                 <Footer/>
             </header>
         </div>
--- src/nav/nav.tsx
+++ src/nav/nav.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import style from './Nav.module.css'
+import {Link} from "react-router-dom";
 
-export const Navi=()=>{
+export const Navi = () => {
     return (
         <div className={style.nav}>
-            <a href={''}>Main</a>
-            <a href={''}>My skills</a>
-            <a href={''}>My projects</a>
-            <a href={''}>Contact me</a>
-
+            <Link to="/main">Main</Link>
+            <Link to="/skills">My skills</Link>
+            <Link to="/works">My projects</Link>
+            <Link to="/contacts">Contact me</Link>
         </div>
     )
 
